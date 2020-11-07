5bd1873e87759e1dd468d14e453fbb2186ad645a
oleksiiPakki <72913648+oleksiiPakki@users.noreply.github.com>
2020-11-07 08:28:50 +0200

    Delete State.java

--- FiniteStateMachine/src/main/java/io/teamdev/javaclasses/impl/math/WrongCountOfArgumentsException.java
+++
@@ -1,9 +0,0 @@
-package io.teamdev.javaclasses.impl.math;
-
-import io.teamdev.javaclasses.impl.fsm.IncorrectFormatOfExpressionException;
-
-public class WrongCountOfArgumentsException extends IncorrectFormatOfExpressionException {
-    public WrongCountOfArgumentsException(String message){
-        super(message);
-    }
-}
